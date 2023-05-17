4f4a48cebdae23576eb0d8aa7ff62cb74dd3027a
bharathchowdary <Saibharath0459@gmail.com>
2023-05-17 10:42:45 -0500

    extra info is added

--- src/main/java/com/amzon/Main.java
+++ src/main/java/com/amzon/Main.java
@@ -4,9 +4,8 @@ package com.amzon;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("how are you");
 
 
     }
